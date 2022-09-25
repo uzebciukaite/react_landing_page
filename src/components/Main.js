acf78ec0c6059f3e9c8d82c5a438c2766f99f95b
uzebciukaite <urtezeb@gmail.com>
2022-09-25 21:12:24 +0300

    deloyed

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,6 @@ const Main = () => {
     <Container id="home" className="d-flex w-100 h-100 main pt-5">
         <Container className="d-flex flex-column justify-content-center mr-4 w-60 gap-3">
           <h4 className='fs-1 font-weight-bold'>landing page</h4>
-          
           <p className='fs-5'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum necessitatibus, ducimus quas consequuntur.</p>
           <div className="d-flex gap-4 buttons">
             <Button className="w-25 py-3 customBtnstyle">Discover</Button>
